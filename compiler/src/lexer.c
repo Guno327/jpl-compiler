1eee4379619cb66c9afea962cfff2272f22f5c2b
Guno327 <gunnarhovik@outlook.com>
2025-01-15 02:31:06 -0700

    lex_pnct (and by proxy lex_ws lex_nl lex_op) are working

--- compiler/src/lexer.c
+++ compiler/src/lexer.c
@@ -4,19 +4,7 @@
 #include <ctype.h>
 #include "lexer.h"
 
-/** 
- * @brief Lexes a string token from src.
- * 
- * This function traverses through src starting at index i.
- * It expect index i to be a '=' as that signifies the beginning of
- * a string token. It then continues until it find the corresponding
- * '"' to end the string, or encounters a new-line or EOF (error)
- * 
- * @param src The string containing the source code to be lexed
- * @param i The index to begin lexing a string at
- * @param t A pointer to a token where to result will be stored
- * @return The ending index of the string (t implicitly updated in function)
- */
+// Lexes a string
 int lex_str(const char* src, int i, Token* t){
   if(src[i] != '"'){
       fprintf(stderr, "Unexpected char '%c' at %d\n", src[i], i);
@@ -42,18 +30,7 @@ int lex_str(const char* src, int i, Token* t){
   exit(EXIT_FAILURE);
 }
 
-/*
-  * @breif Lexes a variable of keyword from src
-  *
-  * This funtion traverses through src starting at index i.
-  * It expects src[i] to be a valid alphabet character.
-  * Continues parsing until space, EOF, or newline occurs.
-  *
-  * @param src The string containing the source code to be lexed
-  * @param i The index to begin lexing at
-  * @param t A pointer to a token where the result will be stored
-  * @return The ending index of the token text (t implicitly updated)
-*/ 
+//lexes a keyword or variable
 int lex_wrd(const char* src, int i, Token* t){
   if (!isalpha(src[i])){
     fprintf(stderr, "Unexpected char '%c' at %d", src[i], i);
@@ -81,81 +58,184 @@ int lex_wrd(const char* src, int i, Token* t){
   return i;
 }
 
+// Lexes an INTVAL or FLOATVAL
 int lex_num(const char* src, int i, Token* t){
   return 0;
 }
 
+// Lexes a new line
 int lex_nl(const char* src, int i, Token* t){
-  return 0;
+  if (src[i] != '\n'){
+      fprintf(stderr, "Unexpected char '%c' at %d\n", src[i], i);
+      exit(EXIT_FAILURE);
+  }
+  t->start = i;
+
+  for(i+=1; i < strlen(src); i++){
+    if (src[i] != '\n' && src[i] != ' ' && src[i] != '\\')
+      break;
+  }
+  t->type = NEWLINE;
+  return i;
 }
 
+//lexes whitespace
 int lex_ws(const char* src, int i, Token* t){
-  return 0;
+  if (src[i] != ' ' || src[i] != '\\'){
+      fprintf(stderr, "Unexpected char '%c' at %d\n", src[i], i);
+      exit(EXIT_FAILURE);
+  }
+  t->start = i;
+
+  for(i+=1; i < strlen(src); i++){
+    if (src[i] != ' ' && src[i] != '\\')
+      break;
+  }
+  t->type = -1;
+  return i;
 }
 
+//lexes an OP
 int lex_op(const char* src, int i, Token* t){
-  return 0;
+  t->start = i;
+  t->type = OP;
+
+  switch (src[i]){
+    case '=':
+      if (i+1 < strlen(src) && src[i+1] == '='){
+        t->text = "==";
+        i += 2;
+      }
+      else{
+        t->text = "=";
+        i += 1;
+      }
+      break;
+    case '<':
+      if (i+1 < strlen(src) && src[i+1] == '='){
+        t->text = "<=";
+        i += 2;
+      }
+      else{
+        t->text = "<";
+        i += 1;
+      }
+      break;
+    case '>':
+      if (i+1 < strlen(src) && src[i+1] == '='){
+        t->text = ">=";
+        i += 2;
+      }
+      else {
+        t->text = ">";
+        i += 1;
+      }
+      break;
+    case '!':
+      if (i+1 < strlen(src) && src[i+1] == '='){
+        t->text = "!=";
+        i += 2;
+      }
+      else {
+        t->text = "!";
+        i += 1;
+      }
+      break;
+    case '&':
+      if (i+1 < strlen(src) && src[i+1] == '&'){
+        t->text = "&&";
+        i += 2;
+      }
+      else {
+        fprintf(stderr, "Unexpected char '%c' at %d\n", src[i], i);
+        exit(EXIT_FAILURE);
+      }
+      break;
+    case '|':
+      if (i+1 < strlen(src) && src[i+1] == '|'){
+        t->text = "||";
+        i += 2;
+      }
+      else {
+        fprintf(stderr, "Unexpected char '%c' at %d\n", src[i], i);
+        exit(EXIT_FAILURE);
+      }
+      break;
+    case '-':
+    case '+':
+    case '*':
+    case '%':
+    case '/':
+      t->text = malloc(2);
+      strncpy(t->text, src + i, 1);
+      i += 1;
+      break;
+    default:
+      fprintf(stderr, "Unexpected char '%c' at %d\n", src[i], i);
+      exit(EXIT_FAILURE);
+  }
+  return i;
 }
 
+//lexes single char tokens (pnct | nl | ws | op)
 int lex_pnct(const char* src, int i, Token* t){
-  int chars = 0;
-
   switch (src[i]){
   // Check punctuation
     case ':': 
       t->type = COLON; 
       t->text = ":"; 
-      chars = 1;
+      i = 1;
       break; 
     case ',': 
       t->type = COMMA; 
       t->text = ",";
-      chars = 1;
+      i = 1;
       break;
     case '.': 
       t->type = DOT; 
       t->text = "."; 
-      chars = 1;
+      i = 1;
       break;
     case '{': 
       t->type = LCURLY; 
       t->text = "{"; 
-      chars = 1;
+      i = 1;
       break;
     case '}': 
       t->type = RCURLY; 
       t->text = "}"; 
-      chars = 1;
+      i = 1;
       break;
     case '(': 
       t->type = LPAREN; 
       t->text = "("; 
-      chars = 1;
+      i = 1;
       break;
     case ')': 
       t->type = RPAREN; 
       t->text = ")"; 
-      chars = 1;
+      i = 1;
       break;
     case '[': 
       t->type = LSQUARE;
       t->text = "[";
-      chars = 1;
+      i = 1;
       break;
     case ']': 
       t->type = RSQUARE;
       t->text = "]"; 
-      chars = 1;
+      i = 1;
       break;
   // Handle special single char cases
     case '\n':
-      chars = lex_nl(src, i, t);
+      i = lex_nl(src, i, t);
       break;
     case '"':
-      chars = lex_str(src, i, t);
+      i = lex_str(src, i, t);
       break;
     case ' ':
-      chars = lex_ws(src, i, t);
+    case '\\':
+      i = lex_ws(src, i, t);
       break;
     case '=':
     case '+':
@@ -165,41 +245,48 @@ int lex_pnct(const char* src, int i, Token* t){
     case '&':
     case '|':
     case '!':
-      chars = lex_op(src, i, t);
+    case '*':
+    case '%':
+    case '/':
+      i = lex_op(src, i, t);
       break;
     default:
       fprintf(stderr, "Unexpected char '%c' at %d\n", src[i], i);
       exit(EXIT_FAILURE);
   }
 
-  return chars;
+  return i;
 }
 
 Vector* lex(const char* src) {
 
   Vector* tokens = NULL;
   tokens = malloc(sizeof(Vector));
-  
-  for (int i = 0; i < strlen(src); i++){
+ 
+  int i = 0;
+  while(i < strlen(src)){
     Token* t = malloc(sizeof(Token));
     t->start = i;
 
     // Check letter
     if (isalpha(src[i])){
-      i += lex_wrd(src, i, t);
+      i = lex_wrd(src, i, t);
       vector_append(tokens, t);
       continue;
     }
 
     // Check number
     else if (isdigit(src[i])){
-      i += lex_num(src, i, t);
+      i = lex_num(src, i, t);
       vector_append(tokens, t);
       continue;
     }
-
-    if (t->type != 0){
-      vector_append(tokens, t);
+   
+    // All other
+    else {
+      i = lex_pnct(src, i, t);
+      if (t->type != -1)
+        vector_append(tokens, t);
       continue;
     }
 
