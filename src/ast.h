535de21b993ac4a06a43139753c91306f8b54654
Guno327 <gunnarhovik@outlook.com>
2025-01-28 17:39:03 -0700

    updated ast.h with new types for HW4

--- src/ast.h
+++ src/ast.h
@@ -10,7 +10,9 @@ typedef enum {
   ASSERTCMD,
   PRINTCMD,
   SHOWCMD,
-  TIMECMD
+  TIMECMD,
+  FNCMD,
+  STRUCTCMD
 } CmdType;
 typedef struct {
   int start;
@@ -24,13 +26,46 @@ typedef enum {
   TRUEEXPR,
   FALSEEXPR,
   VAREXPR,
-  ARRAYLITERALEXPR
+  ARRAYLITERALEXPR,
+  VOIDEXPR,
+  STRUCTLITERALEXPR,
+  DOTEXPR,
+  ARRAYINDEXEXPR,
+  CALLEXPR
 } ExprType;
 typedef struct {
   int start;
   ExprType type;
   void *node;
 } Expr;
+
+typedef enum {
+  INTTYPE,
+  BOOLTYPE,
+  FLOATTYPE,
+  ARRAYTYPE,
+  VOIDTYPE,
+  STRUCTTYPE
+} TypeType;
+typedef struct {
+  int start;
+  TypeType type;
+  void *node;
+} Type;
+
+typedef enum { LETSTMT, ASSERTSTMT, RETURNSTMT } StmtType;
+typedef struct {
+  int start;
+  StmtType type;
+  void *node;
+} Stmt;
+
+typedef enum { VARLVALUE, ARRAYLVALUE } LValueType;
+typedef struct {
+  int start;
+  LValueType type;
+  void *node;
+} LValue;
 
 // Special Cases (VAR)
 typedef struct {
@@ -40,14 +75,73 @@ typedef struct {
 
 typedef struct {
   int start;
+  LValue *lval;
+  Type *type;
+} Binding;
+
+// LValue types
+typedef struct {
+  int start;
   char *var;
 } VarLValue;
+
+typedef struct {
+  int start;
+  char *var;
+  int lvals_size;
+  char **lvals;
+} ArrayLValue;
+
+// Stmt types
+typedef struct {
+  int start;
+  LValue *lval;
+  Expr *expr;
+} LetStmt;
+
+typedef struct {
+  int start;
+  Expr *expr;
+  char *str;
+} AssertStmt;
+
+typedef struct {
+  int start;
+  Expr *expr;
+} ReturnStmt;
+
+// TYPE types
+typedef struct {
+  int start;
+} IntType;
+
+typedef struct {
+  int start;
+} BoolType;
+
+typedef struct {
+  int start;
+} FloatType;
+
+typedef struct {
+  int start;
+  int rank;
+} ArrayType;
+
+typedef struct {
+  int start;
+} VoidType;
+
+typedef struct {
+  int start;
+  char *var;
+} StructType;
 
 // CMD types
 typedef struct {
   int start;
   char *str;
-  VarLValue *lvalue;
+  LValue *lvalue;
 } ReadCmd;
 
 typedef struct {
@@ -58,7 +152,7 @@ typedef struct {
 
 typedef struct {
   int start;
-  VarLValue *lvalue;
+  LValue *lvalue;
   Expr *expr;
 } LetCmd;
 
@@ -83,6 +177,24 @@ typedef struct {
   Cmd *cmd;
 } TimeCmd;
 
+typedef struct {
+  int start;
+  char *var;
+  int binds_size;
+  Binding **binds;
+  int stmts_size;
+  Stmt **stmts;
+} FnCmd;
+
+typedef struct {
+  int start;
+  char *var;
+  int vars_size;
+  char **vars;
+  int types_size;
+  Type **types;
+} StructCmd;
+
 // EXPR types
 typedef struct {
   int start;
@@ -104,13 +216,44 @@ typedef struct {
 
 typedef struct {
   int start;
-  int list_size;
-  Expr **list;
+  int exprs_size;
+  Expr **exprs;
 } ArrayLiteralExpr;
+
+typedef struct {
+  int start;
+} VoidExpr;
+
+typedef struct {
+  int start;
+  char *var;
+  int exprs_size;
+  Expr **exprs;
+} StructLiteralExpr;
+
+typedef struct {
+  int start;
+  Expr *expr;
+  char *var;
+} DotExpr;
+
+typedef struct {
+  int start;
+  Expr *expr;
+  int exprs_size;
+  Expr **exprs;
+} ArrayIndexExpr;
+
+typedef struct {
+  int start;
+  char *var;
+  int list_size;
+  Expr *list;
+} CallExpr;
 
 // Methods
 char *print_cmd(Cmd *cmd);
 char *print_expr(Expr *cmd);
-char *print_lvalue(VarLValue *lvalue);
+char *print_lvalue(LValue *lvalue);
 
 #endif
